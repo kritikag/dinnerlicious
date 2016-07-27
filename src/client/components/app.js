8e351314efa4d5ba37f2e2b9fc3f2c39a98baff8
Sharanya Charabuddi <scharabuddi@paypal.com>
2016-07-26 17:37:17 -0700

    starter project

---
+++ src/client/components/app.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import SearchBar from './search_bar';
+import VideoList from './video_list'
+import YTSearch from 'youtube-api-search'
+const API_KEY='AIzaSyAh3gnYhg45PCu6jCf_8j6fAg8FTYHKudk';
+export default class App extends Component {
+
+	constructor(props){
+		super(props);
+		this.state={videos:[]};
+
+		YTSearch({key:API_KEY,term:'surfboards'},(videos)=> {
+			console.log(videos);
+			this.setState({videos});
+		});
+
+	}
+  render() {
+    return (
+      <div>
+      <SearchBar />
+      <VideoList  videos={this.state.videos} />
+      </div>
+    );
+  }
+}
